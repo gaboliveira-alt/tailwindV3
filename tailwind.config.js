72d64f8056931a3e7ad9618f59ecb2e81d8660b3
Gabriel Oliveira Pinto <157530699+gaboliveira-alt@users.noreply.github.com>
2026-03-20 16:06:00 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./src/**/*.{html,js}"];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
-
+module.exports = {
+  darkMode: 'selector',
+  content: ['./src/**/*.{html,js,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}   
